66bf2434c72a918eecb7680ec2292a1889632ab4
Nay Htet Aung <nayhtetaung201733@gmail.com>
2021-09-21 02:28:40 +0630

    Functionality

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,54 @@
 <template>
-   <div id="app">
-     <header>
-       <h1>The<strong>Anime</strong>Database</h1>
-        <form class="search-box">
-       <input type="search" class="search-field" placeholder="Search for an anime..." required>
-     </form>
-     </header>
-    <main>
-      <div class="cards">
-        <Card></Card>
-      </div>
-    </main>
-   </div>
+	<div class="app">
+		<header>
+			<h1>The<strong>Anime</strong>Database</h1>
+
+			<form class="search-box" @submit.prevent="HandleSearch">
+				<input 
+					type="search" 
+					class="search-field" 
+					placeholder="Search for an anime..."
+					required
+					v-model="search_query" />
+			</form>
+		</header>
+		<main>
+			<div class="cards" v-if="animelist.length > 0">
+				<Card 
+					v-for="anime in animelist" 
+					:key="anime.mal_id"
+					:anime="anime" />
+			</div>
+			<div class="no-results" v-else>
+				<h3>Sorry, we have no results...</h3>
+			</div>
+		</main>
+	</div>
 </template>
 
 <script>
-import Card from './components/Card.vue'
+import { ref } from 'vue';
+import Card from './components/Card';
 export default {
-  name: 'App',
-  components:{ Card },
-  setup(){
-    
-  }
+  components:{
+	Card,
+  },
+	setup() {
+		const search_query = ref("");
+		const animelist = ref([]);
+		const HandleSearch = async () => {
+			animelist.value = await fetch(`https://api.jikan.moe/v3/search/anime?q=${search_query.value}`)
+				.then(res => res.json())
+				.then(data => data.results);
+			search_query.value = "";
+		}
+		return {
+		
+			search_query,
+			animelist,
+			HandleSearch
+		}
+	}
 }
 </script>
 
@@ -31,7 +58,6 @@ export default {
 	padding: 0;
 	box-sizing: border-box;
 	font-family: 'Fira Sans', sans-serif;
-  background: #18dcff;
 }
 a {
 	text-decoration: none;
