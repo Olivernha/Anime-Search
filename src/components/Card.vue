be00bc705fd25fc48e384c1947d61becb2db7cca
Nay Htet Aung <nayhtetaung201733@gmail.com>
2021-09-21 02:31:16 +0630

    Finalize

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -26,7 +26,7 @@ export default {
 	img {
 		width: 100%;
 		height: 350px;
-		object-fit: contain;
+		object-fit: fill;
 		border-radius: 16px;
 		box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
 		transition: 0.4s;
