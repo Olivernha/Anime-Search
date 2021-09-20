01769a5b0f7ce7f5fdf748782a4f75f2aaf8f4ab
Nay Htet Aung <nayhtetaung201733@gmail.com>
2021-09-21 02:00:53 +0630

    Styling & Markup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,98 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <div id="app">
+     <header>
+       <h1>The<strong>Anime</strong>Database</h1>
+        <form class="search-box">
+       <input type="search" class="search-field" placeholder="Search for an anime..." required>
+     </form>
+     </header>
+    <main>
+      <div class="cards">
+        <Card></Card>
+      </div>
+    </main>
+   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Card from './components/Card.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{ Card },
+  setup(){
+    
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: 'Fira Sans', sans-serif;
+  background: #18dcff;
+}
+a {
+	text-decoration: none;
+}
+header {
+	padding-top: 50px;
+	padding-bottom: 50px;
+	h1 {
+		color: #888;
+		font-size: 42px;
+		font-weight: 400;
+		text-align: center;
+		text-transform: uppercase;
+		margin-bottom: 30px;
+		strong {
+			color: #313131;
+		}
+		&:hover {
+			color: #313131;
+		}
+	}
+	.search-box {
+		display: flex;
+		justify-content: center;
+		padding-left: 30px;
+		padding-right: 30px;
+		.search-field {
+			appearance: none;
+			background: none;
+			border: none;
+			outline: none;
+			background-color: #F3F3F3;
+			box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+			display: block;
+			width: 100%;
+			max-width: 600px;
+			padding: 15px;
+			border-radius: 8px;
+			color: #313131;
+			font-size: 20px;
+			transition: 0.4s;
+			&::placeholder {
+				color: #AAA;
+			}
+			&:focus, &:valid {
+				color: #FFF;
+				background-color: #313131;
+				box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.15);
+			}
+		}
+	}
+}
+main {
+	max-width: 1200px;
+	margin: 0 auto;
+	padding-left: 30px;
+	padding-right: 30px;
+	.cards {
+		display: flex;
+		flex-wrap: wrap;
+		margin: 0 -8px;
+	}
 }
 </style>
